8f7fa7c5672edff249d9289185b840a528ebfbbe
jenniferstarkey <jenniferrstarkey@yahoo.com>
2020-11-17 11:50:08 -0600

    part one complete. printing building info

---
+++ Building.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace Planner
+{
+
+    public class Building
+    {
+        private string _designer = "Starkey";
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume
+        {
+            get
+            {
+                return Width * Depth * (3 * Stories);
+            }
+        }
+
+        public Building(string address)
+        {
+            _address = address;
+        }
+
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+
+        public void Purchase(string person)
+        {
+            _owner = person;
+        }
+        public string AllBuildingInfo()
+        {
+            return $@"
+            {_address}
+            ---------------
+            Designed by {_designer}
+            Constructed on {_dateConstructed}
+            Owned by {_owner}";
+        }
+    }
+}
